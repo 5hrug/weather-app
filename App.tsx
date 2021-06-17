bcb55b75430d9d581077213651dd2f4582ddf8ba
Rastislav <rastouhrin1@gmail.com>
2021-06-17 07:14:30 +0200

    first tries

--- App.tsx
+++ App.tsx
@@ -1,22 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 import MainScreen from './src/Screens/MainScreen';
+import Constants from 'expo-constants';
+import { SwipeablePanel } from 'rn-swipeable-panel';
+
 export default function App() {
+  const [panelProps, setPanelProps] = useState({
+    fullWidth: true,
+    openLarge: true,
+    showCloseButton: true,
+    onClose: () => closePanel(),
+    onPressCloseButton: () => closePanel(),
+    // ...or any prop you want
+  });
+  const [isPanelActive, setIsPanelActive] = useState(false);
+
+  const openPanel = () => {
+    setIsPanelActive(true);
+  };
+
+  const closePanel = () => {
+    setIsPanelActive(false);
+  };
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <MainScreen/>
-
+      {/* <SwipeablePanel {...panelProps} isActive={isPanelActive}>
+        <Text>hey</Text>
+      </SwipeablePanel> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      height: '100%',
+
+      marginTop: Platform.OS === 'android'? Constants.statusBarHeight : 0, 
+   },
 });
--- src/Screens/MainScreen.tsx
+++ src/Screens/MainScreen.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import {View} from 'react-native';
-function MainScreen() {
-  return (
-    <View>
-      
-    </View>
-  )
-}
+import React from 'react';
+import { View, StyleSheet, ImageBackground, Text } from 'react-native';
+import { Image } from 'react-native-elements';
 
-export default MainScreen
+function MainScreen() {
+   return (
+      <View style={styles.container}>
+         {/* <Image
+            style={styles.graphic}
+            source={require('../../assets/graphic.jpg')} 
+         /> */}
+         <ImageBackground
+            source={require('../../assets/graphic.jpg')}
+            style={styles.graphic}
+            resizeMethod="resize"
+ 
+            imageStyle={{ 
+               // resizeMode: 'contain',
+               resizeMode: 'contain',
+
+
+            }}></ImageBackground>
+         {/* <Image source={require('../../assets/graphic.jpg')} style={styles.graphic} /> */}
+         {/* <Text style={styles.text}> aspon daco</Text> */}
+      </View>
+   );
+}
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      height: '100%',
+      overflow: 'hidden',
+      // justifyContent: 'flex-start',
+      // alignItems: 'flex-start',
+   },
+   graphic: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      height: '100%',
+      // marginTop: -100,
+      top: 0, 
+      left: 0,
+      bottom:0,
+   }, 
+   text: {
+   },
+});
+export default MainScreen;
