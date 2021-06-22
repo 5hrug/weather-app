720fa2ec80bdfad0db6fd6f95742071655e94c0c
Rastislav <rastouhrin1@gmail.com>
2021-06-22 15:12:21 +0200

    axios context also working with tsx

--- src/Components/Context.tsx
+++ src/Components/Context.tsx
@@ -2,8 +2,8 @@ import React, { useState, useContext } from 'react';
 import { useEffect } from 'react';
 const axios = require('axios').default;
 
-// export const Context = React.createContext<any | undefined>(undefined);
-export const Context = React.createContext();
+export const Context = React.createContext<any | undefined>(undefined);
+// export const Context = React.createContext();
 
 export function useTodoContext() {
    const {data} = useContext(Context)
@@ -14,22 +14,22 @@ export function useTodoContext() {
 }
  
 
-const ContextProvider = ({ children }) => {
-   const [data, setData] = useState();
+const ContextProvider = ({ children }:any) => {
+   const [data, setData] = useState<any | undefined>(undefined);
    const [pressedSearch, setPressedSearch] = useState(true);
 
    const pressHandler = () => {
       setPressedSearch(!pressedSearch);
    };
 
-   const pressedCity = (city) => { 
+   const pressedCity = (city:any) => { 
       console.log(city);
       const town = city ?? 'Bratislava';
       axios
          .get(
             `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=29501213cfe466764c7b2b83e1e9506e&units=metric`
          )
-         .then(function (response) {
+         .then(function (response:any) {
             setData(response.data);
             console.log('dalo set data');
          });
